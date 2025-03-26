7ca5c376e25f80ed33b2086b585f4cf9cc7171f5
bayelaye313 <bayelaye0510@gmail.com>
2025-03-26 21:33:41 +0000

    feat style with tailwind

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <p className="font-bold">pooool</p>
+      <p className="font-bold underline text-5xl text-green-600">pooool</p>
     </div>
   );
 }
